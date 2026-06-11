f4cf25f85e28dcc2068882bbccb8bb756b5afc81
devamir <devamir99@gmail.com>
2026-06-11 21:34:36 +0330

    initial commit

---
+++ database/migrations/2026_06_11_172011_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Enums\PostStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('category_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('excerpt')->nullable();
+            $table->text('content')->nullable();
+            $table->string('cover_image')->nullable();
+            $table->enum('status', array_column(PostStatus::cases(), 'value'))->default(PostStatus::DRAFT->value);
+            $table->integer('views')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
