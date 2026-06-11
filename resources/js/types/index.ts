d896674631c41fa96be05b0e144d4fecd75ac56f
devamir <devamir99@gmail.com>
2026-06-11 22:24:50 +0330

    Export blog domain types from types index

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -1,3 +1,6 @@
 export type * from './auth';
+export type * from './category';
 export type * from './navigation';
+export type * from './post';
+export type * from './tag';
 export type * from './ui';
