d65ff46e7debaaab7509c40b92c3d3612760819e
devamir <devamir99@gmail.com>
2026-06-11 22:32:54 +0330

    Fix posts route order so create page does not match show route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::inertia('/', 'welcome')->name('home');
 
 Route::inertia('posts', 'posts/index')->name('posts.index');
-Route::inertia('posts/{post}', 'posts/show')->name('posts.show');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
@@ -14,5 +13,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('categories', 'categories/index')->name('categories.index');
     Route::inertia('tags', 'tags/index')->name('tags.index');
 });
+
+Route::inertia('posts/{post}', 'posts/show')->name('posts.show');
 
 require __DIR__.'/settings.php';
