8079e8a16cf38a8f680f358a36b5ad1a6d1c8537
devamir <devamir99@gmail.com>
2026-06-11 22:32:48 +0330

    Enable HasFactory on Tag model

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,16 +2,20 @@
 
 namespace App\Models;
 
+use Database\Factories\TagFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 #[Fillable(['name', 'slug'])]
 class Tag extends Model
 {
-    public function posts() : BelongsToMany
+    /** @use HasFactory<TagFactory> */
+    use HasFactory;
+
+    public function posts(): BelongsToMany
     {
         return $this->belongsToMany(Post::class);
     }
-
 }
