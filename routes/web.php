a0c6dd20b0665f5f7295269d6f307d29516ab47e
devamir <devamir99@gmail.com>
2026-06-11 22:24:55 +0330

    Register blog post, category, and tag routes in web routes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
+Route::inertia('posts', 'posts/index')->name('posts.index');
+Route::inertia('posts/{post}', 'posts/show')->name('posts.show');
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    Route::inertia('posts/create', 'posts/create')->name('posts.create');
+    Route::inertia('posts/{post}/edit', 'posts/edit')->name('posts.edit');
+    Route::inertia('categories', 'categories/index')->name('categories.index');
+    Route::inertia('tags', 'tags/index')->name('tags.index');
 });
 
 require __DIR__.'/settings.php';
