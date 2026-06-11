0a72cb2f1d18eb86492cf267f287b473cd097dd1
devamir <devamir99@gmail.com>
2026-06-11 21:48:13 +0330

    Add fillable fields to Tag model to support Lighthouse create and update

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+#[Fillable(['name', 'slug'])]
 class Tag extends Model
 {
     public function posts() : BelongsToMany
